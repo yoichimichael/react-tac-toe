d4ef981bd3713912c6b9bb105c2fd73fd3ec4267
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-04-15 14:47:58 -0400

    refactor Square button onClick handler

--- src/index.js
+++ src/index.js
@@ -3,18 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      value: null
+    };
+  }
+
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button 
+        className="square" 
+        onClick={() => this.setState({value: 'X'})}
+      >
+        {/* Board component passes 'value' props to Square component */}
+        {/* 'this' refers to Board element object */}
+        {this.state.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+  // pass parameter 'i' as 'value' props to Square component
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i}/>;
   }
 
   render() {
@@ -24,6 +37,7 @@ class Board extends React.Component {
       <div>
         <div className="status">{status}</div>
         <div className="board-row">
+          {/* all renderSquare calls pass an integer corresponding to square num, left to right, top to bottom */}
           {this.renderSquare(0)}
           {this.renderSquare(1)}
           {this.renderSquare(2)}
